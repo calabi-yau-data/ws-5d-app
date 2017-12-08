a0244db0d4a1a0ef1dd524298dea8233007ce138
Friedrich Schöller <code@schoeller.se>
2017-12-08 21:08:18 +0100

    Add hash to filename

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ const extractSass = new ExtractTextPlugin({
 module.exports = {
     entry: "./src/app.js",
     output: {
-        filename: "bundle.js",
+        filename: "bundle.[hash].js",
         path: path.resolve(__dirname, "build"),
     },
     module: {
